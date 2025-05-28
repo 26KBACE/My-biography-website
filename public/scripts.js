87fa29b9a5fd4cd78a7791817f2d7fe428e9ebdf
Brian Mong'are <brianmongare207@gmail.com>
2025-05-28 11:28:47 +0300

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -9,7 +9,7 @@ window.onload = function() {
       50% { opacity: 0; }
     }
     .blinking {
-      animation: blink 1s infinite;
+      animation: blink 5s infinite;
     }
   `;
   document.head.appendChild(style);
